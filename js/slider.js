9ad4aa81077e78c1d75f40c865eef4ca5d244d56
Dimas Indra Jaya <dimaszxc101@gmail.com>
2025-01-02 06:24:43 +0700

    update

--- js/slider.js
+++ js/slider.js
@@ -4,11 +4,11 @@ Glide Js Carousel
 =============
  */
 
-const slider1 = document.getElementById("glide_1");
-const slider2 = document.getElementById("glide_2");
-const slider3 = document.getElementById("glide_3");
-const slider4 = document.getElementById("glide_4");
-const slider5 = document.getElementById("glide_5");
+const slider1 = document.getElementById('glide_1');
+const slider2 = document.getElementById('glide_2');
+const slider3 = document.getElementById('glide_3');
+const slider4 = document.getElementById('glide_4');
+const slider5 = document.getElementById('glide_5');
 
 /*
 =============
@@ -16,15 +16,15 @@ Hero
 =============
  */
 if (slider1) {
-  new Glide(slider1, {
-    type: "carousel",
-    startAt: 0,
-    autoplay: 3000,
-    hoverpause: true,
-    perView: 1,
-    animationDuration: 800,
-    animationTimingFunc: "linear",
-  }).mount();
+    new Glide(slider1, {
+        type: 'carousel',
+        startAt: 0,
+        autoplay: 3000,
+        hoverpause: true,
+        perView: 1,
+        animationDuration: 800,
+        animationTimingFunc: 'linear',
+    }).mount();
 }
 
 /*
@@ -34,22 +34,22 @@ Latest Products
  */
 
 if (slider2) {
-  new Glide("#glide_2", {
-    type: "carousel",
-    startAt: 0,
-    perView: 4,
-    rewin: false,
-    animationDuration: 800,
-    animationTimingFunc: "cubic-bezier(0.165, 0.840, 0.440, 1.000)",
-    breakpoints: {
-      1200: {
-        perView: 3,
-      },
-      768: {
-        perView: 2,
-      },
-    },
-  }).mount();
+    new Glide('#glide_2', {
+        type: 'carousel',
+        startAt: 0,
+        perView: 4,
+        rewin: false,
+        animationDuration: 800,
+        animationTimingFunc: 'cubic-bezier(0.165, 0.840, 0.440, 1.000)',
+        breakpoints: {
+            1200: {
+                perView: 3,
+            },
+            768: {
+                perView: 2,
+            },
+        },
+    }).mount();
 }
 
 /*
@@ -59,56 +59,56 @@ Related Products
  */
 
 if (slider3) {
-  new Glide("#glide_3", {
-    type: "carousel",
-    startAt: 0,
-    perView: 4,
-    rewin: false,
-    animationDuration: 800,
-    animationTimingFunc: "cubic-bezier(0.165, 0.840, 0.440, 1.000)",
-    breakpoints: {
-      1200: {
-        perView: 3,
-      },
-      768: {
-        perView: 2,
-      },
-    },
-  }).mount();
+    new Glide('#glide_3', {
+        type: 'carousel',
+        startAt: 0,
+        perView: 4,
+        rewin: false,
+        animationDuration: 800,
+        animationTimingFunc: 'cubic-bezier(0.165, 0.840, 0.440, 1.000)',
+        breakpoints: {
+            1200: {
+                perView: 3,
+            },
+            768: {
+                perView: 2,
+            },
+        },
+    }).mount();
 }
 
 // Testimonial
 
 if (slider4) {
-  new Glide("#glide_4", {
-    type: "carousel",
-    startAt: 0,
-    perView: 1,
-    rewin: false,
-    animationDuration: 800,
-    animationTimingFunc: "cubic-bezier(0.165, 0.840, 0.440, 1.000)",
-  }).mount();
+    new Glide('#glide_4', {
+        type: 'carousel',
+        startAt: 0,
+        perView: 1,
+        rewin: false,
+        animationDuration: 800,
+        animationTimingFunc: 'cubic-bezier(0.165, 0.840, 0.440, 1.000)',
+    }).mount();
 }
 
 // News
 if (slider5) {
-  new Glide("#glide_5", {
-    type: "carousel",
-    startAt: 0,
-    perView: 3,
-    rewin: false,
-    autoplay: 3000,
-    animationDuration: 800,
-    animationTimingFunc: "cubic-bezier(0.165, 0.840, 0.440, 1.000)",
-    breakpoints: {
-      998: {
-        perView: 2,
-      },
-      768: {
-        perView: 1,
-      },
-    },
-  }).mount();
+    new Glide('#glide_5', {
+        type: 'carousel',
+        startAt: 0,
+        perView: 3,
+        rewin: false,
+        autoplay: 3000,
+        animationDuration: 800,
+        animationTimingFunc: 'cubic-bezier(0.165, 0.840, 0.440, 1.000)',
+        breakpoints: {
+            998: {
+                perView: 2,
+            },
+            768: {
+                perView: 1,
+            },
+        },
+    }).mount();
 }
 
 AOS.init();
